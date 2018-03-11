fb5e1f56b105e1513b66efcb3ee626f7b04821e2
codemass <worklette@yandex.ru>
2018-03-12 02:06:49 +0300

    Добавлен код 1 вариант

--- app/src/main/java/mydom/fuel/MainActivity.java
+++ app/src/main/java/mydom/fuel/MainActivity.java
@@ -2,6 +2,10 @@ package mydom.fuel;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onButtonClick (View v) {
+        SeekBar FuelBalance = (SeekBar)findViewById(R.id.seekBarFuel);
+        EditText Price = (EditText) findViewById(R.id.fuelPrice);
+        TextView Total = (TextView) findViewById(R.id.result);
+
+        int Balance = FuelBalance.getProgress();
+        int PriceFl = Integer.parseInt(Price.getText().toString());
+        int Sum = (43-((Balance*43)/12))*PriceFl;
+        Total.setText(Sum);
     }
 }
