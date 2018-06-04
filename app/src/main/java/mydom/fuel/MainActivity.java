3ef913f82672cb1eb86a3f3fc755ad5637d9e712
codemass <worklette@yandex.ru>
2018-06-04 03:27:51 +0300

    Final. Мелкие недочеты. Уменьшил контейнер цены (налегало на индикатор), выставил макс кол-во вводимых символов 5 в цене.

--- app/src/main/java/mydom/fuel/MainActivity.java
+++ app/src/main/java/mydom/fuel/MainActivity.java
@@ -1,6 +1,7 @@
 package mydom.fuel;
 
 import android.graphics.Color;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -21,15 +22,17 @@ public class MainActivity extends AppCompatActivity {
         EditText Price = (EditText) findViewById(R.id.fuelPrice);
         TextView FuelCurrentText = (TextView) findViewById(R.id.textView2);
         TextView Total = (TextView) findViewById(R.id.result);
+        int textColor = ContextCompat.getColor(this, R.color.solarisWhite); //Получаем доступ в color.xml к нужному цвету
 
 
 
         if (Price.getText().toString().trim().isEmpty()) {  //trim() - удяляем начальные и конечные пробелы, isEmpty() - проверка на пустоту
             FuelCurrentText.setText("Введите цену бензина"); //Выводит надпись, если цена пустая
-            FuelCurrentText.setTextColor(Color.RED); //Назначает этой надписи цвет
+            FuelCurrentText.setTextColor(Color.parseColor("#ff4d4d")); //Назначает этой надписи цвет
 
         }
         else {
+            FuelCurrentText.setTextColor(textColor); //Это делается затем, что выше, в if срабатывает изменение цвета текста и сколько в баке уже выводит красным. Так возвращаем solarisWhite.
 
             float Balance = FuelBalance.getProgress();
             float PriceFl = Float.parseFloat(Price.getText().toString());
